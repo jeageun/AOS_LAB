795fcb7505d64a57ad36dc5a08d49350ad61e13d
jeageunjung <jeageunjung@gmail.com>
2020-09-30 17:26:41 +0000

    AOS_tutorial_start

---
+++ AOS2/server.c
@@ -0,0 +1,66 @@
+// Server side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT     8089
+#define MAXLINE 1024
+
+// Driver code
+int main() {
+        int sockfd;
+        char buffer[MAXLINE];
+        char *hello = "Hello from server";
+        struct sockaddr_in servaddr, cliaddr;
+
+        // Creating socket file descriptor
+        if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+                perror("socket creation failed");
+                exit(EXIT_FAILURE);
+        }
+
+        memset(&servaddr, 0, sizeof(servaddr));
+        memset(&cliaddr, 0, sizeof(cliaddr));
+
+        // Filling server information
+        servaddr.sin_family = AF_INET; // IPv4
+        servaddr.sin_addr.s_addr = INADDR_ANY;
+        servaddr.sin_port = htons(PORT);
+
+        // Bind the socket with the server address
+        if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+                        sizeof(servaddr)) < 0 )
+        {
+                perror("bind failed");
+                exit(EXIT_FAILURE);
+        }
+
+        int len, n;
+
+        len = sizeof(cliaddr); //len is value/resuslt
+
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                                &len);
+        buffer[n] = '\0';
+        printf("Client : %s\n", buffer);
+        printf("ip : %s\n",inet_ntoa(cliaddr.sin_addr));
+        sendto(sockfd, (const char *)hello, strlen(hello),
+                MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+                        len);
+
+        printf("Hello message sent.\n");
+        for(int tmp =0;tmp<1000;tmp++){
+        sendto(sockfd, (const char *)hello, strlen(hello),
+                MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+                        len);
+        sleep(1);
+        }
+        return 0;
+}
+
